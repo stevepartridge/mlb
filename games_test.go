cd9743ec68e6c8569a2b4fb94c8ffc57d817db9d
Steve Partridge <srpartridge@gmail.com>
2018-04-15 12:12:01 -0700

    More games tests

--- games_test.go
+++ games_test.go
@@ -7,20 +7,75 @@ import (
 	"github.com/stevepartridge/mlb"
 )
 
-func Test_GetGamesForDate_Success(t *testing.T) {
-	mlbApi, err := mlb.New()
-	if err != nil {
-		t.Error("Should not see error:" + err.Error())
-	}
-	if mlbApi == nil {
-		t.Error("mlbApi should not be nil.")
-	}
+const (
+	validDate1   = "2017/05/17"
+	invalidDate1 = "00/01/01"
+	validTeam1   = mlb.Dodgers
+)
 
-	mlbApi.Debug = true
+func Test_GetGamesByDate_Success(t *testing.T) {
 
-	date, _ := time.Parse("2006/01/02", "2017/05/17")
+	date, _ := time.Parse("2006/01/02", validDate1)
 
 	games, err := mlbApi.GetGamesByDate(date)
+	if err != nil {
+		t.Error("Should not error: " + err.Error())
+	}
+
+	if len(games) == 0 {
+		t.Error("Games should be more than zero.")
+	}
+}
+
+func Test_GetGamesByDate_Failure(t *testing.T) {
+
+	date, _ := time.Parse("2006/01/02", invalidDate1)
+
+	games, err := mlbApi.GetGamesByDate(date)
+	if err == nil {
+		t.Error("Should be an error")
+	}
+
+	if len(games) > 0 {
+		t.Error("Games should not be more than zero.")
+	}
+}
+
+func Test_GetGamesByDateRange_Failure_InvalidEndDate(t *testing.T) {
+
+	start, _ := time.Parse("2006/01/02", validDate1)
+	end, _ := time.Parse("2006/01/02", invalidDate1)
+
+	games, err := mlbApi.GetGamesByDateRange(start, end)
+	if err == nil {
+		t.Error("Should be an error")
+	}
+
+	if len(games) > 0 {
+		t.Error("Games should not be more than zero.")
+	}
+}
+
+func Test_GetGamesByDateForTeam_Success(t *testing.T) {
+
+	date, _ := time.Parse("2006/01/02", validDate1)
+
+	games, err := mlbApi.GetGamesByDateForTeam(date, validTeam1)
+	if err != nil {
+		t.Error("Should not error: " + err.Error())
+	}
+
+	if len(games) == 0 {
+		t.Error("Games should be more than zero.")
+	}
+}
+
+func Test_GetGamesByDateRangeForTeam_Success(t *testing.T) {
+
+	start, _ := time.Parse("2006/01/02", validDate1)
+	end, _ := time.Parse("2006/01/02", validDate1)
+
+	games, err := mlbApi.GetGamesByDateRangeForTeam(start, end, validTeam1)
 	if err != nil {
 		t.Error("Should not error: " + err.Error())
 	}
