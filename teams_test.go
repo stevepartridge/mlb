cc1ff9f69b4ab5f5b2d89f1b4d7e2c201414a39c
Steve Partridge <srpartridge@gmail.com>
2018-04-15 12:45:36 -0700

    Add test to verify team IDs have not changed

--- teams_test.go
+++ teams_test.go
@@ -61,3 +61,58 @@ func Test_GetTeam_Failure_InvalidTeamId(t *testing.T) {
 	}
 
 }
+
+func Test_GetEachTeam_Success(t *testing.T) {
+	testTeams := map[int]string{
+		mlb.Angels:       "Los Angeles Angels",
+		mlb.Diamondbacks: "Arizona Diamondbacks",
+		mlb.Orioles:      "Baltimore Orioles",
+		mlb.RedSox:       "Boston Red Sox",
+		mlb.Cubs:         "Chicago Cubs",
+		mlb.Reds:         "Cincinnati Reds",
+		mlb.Indians:      "Cleveland Indians",
+		mlb.Rockies:      "Colorado Rockies",
+		mlb.Tigers:       "Detroit Tigers",
+		mlb.Astros:       "Houston Astros",
+		mlb.Royals:       "Kansas City Royals",
+		mlb.Dodgers:      "Los Angeles Dodgers",
+		mlb.Nationals:    "Washington Nationals",
+		mlb.Mets:         "New York Mets",
+		mlb.Athletics:    "Oakland Athletics",
+		mlb.Pirates:      "Pittsburgh Pirates",
+		mlb.Padres:       "San Diego Padres",
+		mlb.Mariners:     "Seattle Mariners",
+		mlb.Giants:       "San Francisco Giants",
+		mlb.Cardinals:    "St. Louis Cardinals",
+		mlb.Rays:         "Tampa Bay Rays",
+		mlb.Rangers:      "Texas Rangers",
+		mlb.BlueJays:     "Toronto Blue Jays",
+		mlb.Twins:        "Minnesota Twins",
+		mlb.Phillies:     "Philadelphia Phillies",
+		mlb.Braves:       "Atlanta Braves",
+		mlb.WhiteSox:     "Chicago White Sox",
+		mlb.Marlins:      "Miami Marlins",
+		mlb.Yankees:      "New York Yankees",
+		mlb.Brewers:      "Milwaukee Brewers",
+	}
+
+	for id, team := range testTeams {
+
+		teams, err := mlbApi.GetTeams(id)
+		if err != nil {
+			t.Error("Should not error when retrieving teams")
+		}
+
+		if len(teams) != 1 {
+			t.Error("Should be exactly 1 team found.")
+		}
+
+		if teams[0].Name != team {
+			t.Errorf("Team found (%s) does not match expected team (%s)",
+				teams[0].Name,
+				teams,
+			)
+		}
+	}
+
+}
