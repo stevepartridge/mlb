b052d0d7063eca748732b376e9f81ae1bde7a485
Steve Partridge <srpartridge@gmail.com>
2018-04-15 12:13:26 -0700

    Make call public for optional use

--- api.go
+++ api.go
@@ -8,29 +8,13 @@ import (
 	"net/http"
 	"net/url"
 	"strconv"
+	"strings"
 	"time"
-)
-
-const (
-	baseApiURL = "https://statsapi.mlb.com/api/v1"
 )
 
 // schedule?lang=en&sportId=1&season=2018&startDate=2018-08-01&endDate=2018-08-31&teamId=119&eventTypes=primary&scheduleTypes=games
 
-type Mlb struct {
-	Debug bool
-}
-
-type Response struct {
-	Copyright            string `json:"copyright"`
-	TotalItems           int    `json:"totalItems"`
-	TotalEvents          int    `json:"totalEvents"`
-	TotalGames           int    `json:"totalGames"`
-	TotalGamesInProgress int    `json:"totalGamesInProgress"`
-	Dates                []Date `json:"dates,omitempty"`
-}
-
-func (m *Mlb) call(endpoint string, query map[string]string) (Response, error) {
+func (m *Mlb) Call(endpoint string, query map[string]string) (Response, error) {
 
 	result := Response{}
 
@@ -39,20 +23,46 @@ func (m *Mlb) call(endpoint string, query map[string]string) (Response, error) {
 		Timeout: timeout,
 	}
 
-	_url := fmt.Sprintf("%s/%s?sportId=1", baseApiURL, endpoint)
+	if len(endpoint) < 2 {
+		return result, errors.New("Invalid endpoint")
+	}
+
+	if string(endpoint[0]) == "/" {
+		endpoint = endpoint[1:]
+	}
+
+	u := fmt.Sprintf("%s/%s",
+		baseApiURL,
+		endpoint,
+	)
 
 	if query["lang"] == "" {
 		query["lang"] = "en"
 	}
 
-	for k, v := range query {
-		m.log(" - ", k, v)
-		_url = _url + "&" + k + "=" + url.QueryEscape(v)
+	if query["sportId"] == "" {
+		query["sportId"] = "1"
 	}
 
-	m.log("calling:", _url)
+	for k, v := range query {
+		m.log(" - ", k, v)
+		if !strings.Contains(u, "?") {
+			u = u + "?" + k + "=" + url.QueryEscape(v)
+			continue
+		}
+		u = u + "&" + k + "=" + url.QueryEscape(v)
+	}
 
-	resp, err := client.Get(_url)
+	_url, err := url.Parse(u)
+
+	if err != nil {
+		ifError(err)
+		return result, err
+	}
+
+	m.log("calling:", _url.String())
+
+	resp, err := client.Get(_url.String())
 
 	if err != nil {
 		return result, err
@@ -67,18 +77,26 @@ func (m *Mlb) call(endpoint string, query map[string]string) (Response, error) {
 		return result, err
 	}
 
+	if body != nil {
+		err = json.Unmarshal([]byte(body), &result)
+		if err != nil {
+			ifError(err)
+			return result, err
+		}
+
+	}
+
 	m.log("status code:", resp.StatusCode)
-	if resp.StatusCode != 200 {
+	switch resp.StatusCode {
+	case 200:
+		return result, nil
+	case 404:
+		return result, errors.New("Error (Not Found): " + result.Message)
+	default:
 		return result, errors.New("Invalid response code from MLB.com, StatusCode: " + strconv.Itoa(resp.StatusCode))
 	}
 
-	m.log(" - body: \n %s", string(body))
-
-	err = json.Unmarshal([]byte(body), &result)
-	if err != nil {
-		ifError(err)
-		return result, err
-	}
+	// m.log(" - body: \n %s", string(body))
 
 	return result, nil
 
